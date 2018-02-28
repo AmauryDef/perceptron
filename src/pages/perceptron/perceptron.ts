3b51130efe2f8614651d8fc164b88a7c7ac06ac8
Amaury DEFRANCE <amaury.defrance@epsi.fr>
2018-02-28 09:50:32 +0100

    Working - Missing View display

--- src/pages/perceptron/perceptron.ts
+++ src/pages/perceptron/perceptron.ts
@@ -29,7 +29,7 @@ export class PerceptronPage {
 		let loading = this.loadingCtrl.create({
 	    	spinner: 'hide',
 	    	content: `Please wait...`,
-	    	duration: 5000
+	    	duration: 2000
 	  	});
 
 	  	loading.onDidDismiss(() => {
@@ -39,8 +39,7 @@ export class PerceptronPage {
 	  	loading.present();	
 	  	
 		let entries = [[1,0,0],[1,0,1],[1,1,0],[1,1,1]];
-		let wArray = [{w0:0,w1:0,w2:0,entry:[0,0,0],SomWiXi:0, y:0, cible:0},{w0:global.poids.w0,w1:global.poids.w1,w2:global.poids.w2,entry:[1,0,0],SomWiXi:global.poids.w0, y:null, cible:0}];
-	    console.log(wArray);
+		let wArray = [{w0:0,w1:0,w2:0,entry:[0,0,0],SomWiXi:0, y:0, cible:0},{w0:global.poids.w0,w1:global.poids.w1,w2:global.poids.w2}];
 
 	    var etape = 1;
 	    var entryIndex = 0;
@@ -60,6 +59,8 @@ export class PerceptronPage {
 				newWixi = newWixi + wArray[etape].w2;
 			}
 
+			wArray[etape].SomWiXi = newWixi;
+
 			//Déduction de la variable y à partir de la somme Wixi, si > 0, y =1, sinon y=0
 			if(newWixi >= 0){
 				var y = 1;
@@ -67,12 +68,16 @@ export class PerceptronPage {
 				var y = 0;
 			}
 
+			wArray[etape].y = y;
+
 			//Déduction de la cible (notre perceptron doit apprendre un ET logique, donc la cible sera de 1 seulement si on a toutes nos entrées à 1)
 			if(wArray[etape].entry[0]==1 && wArray[etape].entry[1]==1 && wArray[etape].entry[2]==1){
 				var cible = 1;
 			}else{
 				var cible = 0
 			}
+
+			wArray[etape].cible = cible;
 
 			var newW0 = parseFloat(wArray[etape].w0) + (parseFloat(global.n) * (cible - y) * parseFloat(wArray[etape].entry[0]));
 			var newW1 = parseFloat(wArray[etape].w1) + (parseFloat(global.n) * (cible - y) * parseFloat(wArray[etape].entry[1]));
@@ -91,12 +96,29 @@ export class PerceptronPage {
 
 			wArray.push({w0:newW0,w1:newW1,w2:newW2});
 
-			console.log(wArray);
+			var previousStep = etape-1;
 
-	    }while(wArray[etape].w0 === wArray[etape-1].w0 && wArray[etape].w1 === wArray[etape-1].w1 && wArray[etape].w2 === wArray[etape-1].w2);
+			var check = 0;
+
+			if(wArray[etape].w0 == wArray[previousStep].w0){
+				check++;
+			}
+			if(wArray[etape].w1 == wArray[previousStep].w1){
+				check++;
+			}
+			if(wArray[etape].w2 == wArray[previousStep].w2){
+				check++;
+			}
+
+			console.log(check);
+
+	    }while(check!=3);
+
+	    wArray.splice(0,1);
 
 	    console.log(wArray);
-	    this.wArray=wArray;
+
+	    this.wArray=JSON.stringify(wArray);
 
   	}
 
